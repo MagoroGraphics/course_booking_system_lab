f9a420a7beed892885897a7a1856f26880a849c0
Mario <magoro.graphics@gmail.com>
2022-06-30 11:38:04 +0100

    Work on initialising repositories

--- src/main/java/com/codeclan/course_booking_system/course_booking_system/repository/CourseRepository.java
+++ src/main/java/com/codeclan/course_booking_system/course_booking_system/repository/CourseRepository.java
@@ -1,4 +1,7 @@
 package com.codeclan.course_booking_system.course_booking_system.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CourseRepository {
 }
